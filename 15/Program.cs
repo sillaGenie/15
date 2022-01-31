39bd1543785e7cbe864c7e2a396a251a4e93e93c
ADM <ADM@A320M-H-CF>
2022-01-31 03:18:23 +0300

    Добавьте файлы проекта.

---
+++ 15/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = 5;
+            Product[] products = new Product[n];
+            int id = 1;
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Название: ");
+                string name = Console.ReadLine();
+                Console.Write("Цена: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+                products[i] = new Product() { Id = id, Name = name, Price = price  };
+                id++;
+            };
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+            };
+
+            string jsonString1 = JsonSerializer.Serialize(products,options);
+            Console.Write(jsonString1);
+        }
+    }
+    class Product
+    {
+   
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    
+    }
+ }
+    
---
+++ 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            string jsonString = "[{\"Id\": 1,\"Name\": \"капуста\",\"Price\": 40.35},{\"Id\": 2,\"Name\": \"морковь\",\"Price\": 15.26},{\"Id\": 3,\"Name\": \"свекла\",\"Price\": 60.58},{\"Id\": 4,\"Name\": \"лук\",\"Price\": 10.95},{\"Id\": 5,\"Name\": \"чеснок\",\"Price\": 80.65}]";
+  
+         Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+            int maxI = 0;
+           double max = products[0].Price;
+            for (int i = 0;i < products.Length; i++)
+            {
+                if(products[i].Price > max)
+                    max = products[i].Price;
+                maxI = i;
+            }
+            Console.WriteLine(products[maxI].Name);
+        }
+    }
+    class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
